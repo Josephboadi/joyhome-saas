d84b87b6ffce768dc1e97369dc3e658b0639705a
jboadirgt <113521930+jboadirgt@users.noreply.github.com>
2024-06-24 21:43:16 +0100

    Setting Environ Variable

--- src/joyhome/settings.py
+++ src/joyhome/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#od@d5v70%v5=k3+o1an2*815()(y($&i8reyf8foz0v*=w0&'
+SECRET_KEY = config('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == "true"
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+
+print('DEBUG', DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     '.railway.app' # https://joyhome-saas.prod.railway.app
